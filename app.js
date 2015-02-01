10a258aa29a684c3f34245346e79ed010697432d
edgarw19 <edgarallenpoed@gmail.com>
2015-01-31 20:00:53 -0500

    files

---
+++ app.js
@@ -0,0 +1,58 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+
+app.controller('MainCtrl', [
+	'$scope', 'posts',
+	function($scope, posts){//posts must be passed in
+		$scope.posts = posts.posts;
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === ''){return;}
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0,
+				comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}]
+			})};
+			$scope.incrementUpvotes = function(post){
+				post.upvotes += 1;
+			};
+			$scope.test = "blzh";
+		}]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function(){
+			if($scope.body === ''){return;}
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
+		}
+	}]);
+
+app.config(['$stateProvider', '$urlRouterProvider', 
+	function($stateProvider, $urlRouterProvider){
+		$stateProvider.state('home',{
+			url: '/home', 
+			templateUrl: '/home.html',
+			controller: 'MainCtrl' //initializes with this ctrl, no need to specify earlier
+		});
+		$stateProvider.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+		$urlRouterProvider.otherwise('home');//otherwise go here
+	}]);
+
